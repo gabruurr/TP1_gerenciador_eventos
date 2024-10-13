1cf023bc33e3c5d45241b4ecb043458ca736af84
unknown <gabryelaraujo2019@gmail.com>
2024-10-13 14:31:42 -0300

    Criando classe entidade Servico e atributos

---
+++ src/main/java/com/programacaoweb/gerenciador_eventos/entities/Servico.java
@@ -0,0 +1,55 @@
+package com.programacaoweb.gerenciador_eventos.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Servico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public Servico() {
+    }
+
+    public Servico(String nome, String descricao, Double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
