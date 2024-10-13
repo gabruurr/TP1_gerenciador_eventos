151554798eab1f0661a1d5fb3b9d1fd5dc5c7b12
unknown <gabryelaraujo2019@gmail.com>
2024-10-13 15:02:23 -0300

    Criando classe entidade Participante e atributos

---
+++ src/main/java/com/programacaoweb/gerenciador_eventos/entities/Participante.java
@@ -0,0 +1,57 @@
+package com.programacaoweb.gerenciador_eventos.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Participante() {
+    }
+
+    public Participante(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
