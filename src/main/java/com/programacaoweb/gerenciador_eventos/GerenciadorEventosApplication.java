1059f718c18853ebfa960a78b595fd8f96f7deea
unknown <gabryelaraujo2019@gmail.com>
2024-10-13 15:59:36 -0300

    Tentativa de criar pequeno menu inicial

--- src/main/java/com/programacaoweb/gerenciador_eventos/GerenciadorEventosApplication.java
+++ src/main/java/com/programacaoweb/gerenciador_eventos/GerenciadorEventosApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class GerenciadorEventosApplication implements CommandLineRunner {
 
@@ -11,10 +13,22 @@ public class GerenciadorEventosApplication implements CommandLineRunner {
         SpringApplication.run(GerenciadorEventosApplication.class, args);
     }
 
-
     @Override
     public void run(String... args) throws Exception {
-        
+        Scanner sc = new Scanner(System.in);
+        int escolha;
+
+        System.out.println("PÁGINA INICIAL");
+        System.out.println("1 - Eventos");
+        System.out.println("2 - Participantes");
+        System.out.println("3 - Organizadores");
+        System.out.println("4 - Serviços prestados");
+        System.out.println("5 - Fechar o Programa");
+
+        System.out.print("Escolha uma opção: ");
+        escolha = sc.nextInt();
+        sc.nextLine();
+
     }
 }
 
