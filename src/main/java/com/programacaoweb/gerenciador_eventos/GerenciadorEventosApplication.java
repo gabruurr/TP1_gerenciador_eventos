95d397cbdd173e351e70fd470c49de41fe563dd0
unknown <gabryelaraujo2019@gmail.com>
2024-10-13 15:56:07 -0300

    Implementada interface CommandLineRunner e método run() em GerenciadorEventosApplication

--- src/main/java/com/programacaoweb/gerenciador_eventos/GerenciadorEventosApplication.java
+++ src/main/java/com/programacaoweb/gerenciador_eventos/GerenciadorEventosApplication.java
@@ -1,15 +1,20 @@
 package com.programacaoweb.gerenciador_eventos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciadorEventosApplication {
+public class GerenciadorEventosApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciadorEventosApplication.class, args);
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
 
